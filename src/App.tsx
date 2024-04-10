67b26ddd7bd6cce138a0e456c4a0e6f6a971d1c1
Johnson <yeslee1126@gmail.com>
2024-04-10 10:11:27 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { WalletDerive } from './modules/WalletDerive';
 function App() {
   return (
     <div className="App">
+      <div>
+        声明：该网站不存储私钥，安全起见，建议在无痕模式浏览器 +
+        断网情况下使用。
+      </div>
       <WalletDerive />
     </div>
   );
