be5406282e7958d48fefa2e2cb6b5cd0da0aab0d
Johnson <yeslee1126@gmail.com>
2024-04-10 10:12:33 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ function App() {
         声明：该网站不存储私钥，安全起见，建议在无痕模式浏览器 +
         断网情况下使用。
       </div>
+      <div>
+        <a href="https://github.com/YES-Lee/wallet-tools" target="__blank">
+          源码：https://github.com/YES-Lee/wallet-tools
+        </a>
+      </div>
       <WalletDerive />
     </div>
   );
