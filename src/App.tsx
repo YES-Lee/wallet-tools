8b90f82dde54b9b0dc9442e9463049b6684bef56
Johnson <yeslee1126@gmail.com>
2024-04-10 13:19:46 +0800

    style: margin

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { Box } from '@mui/material';
 import { WalletDerive } from './modules/WalletDerive';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        声明：该网站不存储私钥，安全起见，建议在无痕模式浏览器 +
-        断网情况下使用。
-      </div>
-      <div>
-        <a href="https://github.com/YES-Lee/wallet-tools" target="__blank">
-          源码：https://github.com/YES-Lee/wallet-tools
-        </a>
-      </div>
+      <Box my={2}>
+        <div>
+          声明：该网站不存储私钥，安全起见，建议在无痕模式浏览器 +
+          断网情况下使用。
+        </div>
+        <div>
+          <a href="https://github.com/YES-Lee/wallet-tools" target="__blank">
+            源码：https://github.com/YES-Lee/wallet-tools
+          </a>
+        </div>
+      </Box>
       <WalletDerive />
     </div>
   );
